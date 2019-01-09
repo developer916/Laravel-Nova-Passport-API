242269541c974e904a5cc419b1183afeca6f1f33
web developer <web.developer916@gmail.com>
2019-01-09 20:50:32 +0700

    remove files

--- database/factories/MapPresetHourFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(\App\Models\MapPresetHours::class, function (Faker $faker) use ($factory) {
-    return [
-        'map_preset_id' => $factory->create(App\Models\MapPreset::class)
-    ];
-});
